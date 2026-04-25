d85934b8e6763e8c21d74a3afc6e6988532d4e9e
eunice <choc00minteuuu@gmail.com>
2026-04-25 14:27:18 +0700

    Refine features: add task, star

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("To-Do App Ready 🚀");
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+checkboxes.forEach(checkbox => {
+    box.addEventListener('change', () => {
+        if (checkbox.checked) {
+            box.parentElement.style.opacity = '0.5';    
+        } else {
+            box.parentElement.style.opacity = '1';
+        }
+    });
+});
+
+input.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+        addBtn.click();
+    }
+});
